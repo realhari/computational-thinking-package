7be68a8619a677ca282b30b918fb9355f15a3c2d
realhari <43338534+realhari@users.noreply.github.com>
2018-09-18 14:26:22 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int full,half,i,sum=0;
+	scanf("%d%d",&full,&half);
+	for(full=1;full<=half;full++)
+		{
+			sum=sum+full;
+		
+		}
+		printf("%d",sum);
+	return 0;
+}
