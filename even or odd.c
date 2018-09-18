95cc7d77d2bcf0513f693e0a1e68fdcc16ac0ce5
realhari <43338534+realhari@users.noreply.github.com>
2018-09-18 13:28:47 +0530

    Update even or odd.c

--- even or odd.c
+++ even or odd.c
@@ -6,10 +6,7 @@ int main()
     scanf("%d", &number);
     if(number % 2 == 0);
         printf("%d is even.", number);
-    else if
-        printf("%d is odd.",number);
     else
-        printf("%d is invalid.",number);
-
-    return 0;
+        printf("%d is odd.",number);
+        return 0;
 }
