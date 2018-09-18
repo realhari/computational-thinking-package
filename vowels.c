bea0b926e870783a4da8e71fecdce69c470d8eb4
realhari <43338534+realhari@users.noreply.github.com>
2018-09-18 13:35:38 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char in;
+printf("\nEnter any alphabat...");
+scanf("%c",&in);
+switch(in)
+{
+case 'a':printf("\nVovwel");
+break;
+case 'e':printf("\nVovwel");
+break;
+case 'i':printf("\nVovwel");
+break;
+case 'o':printf("\nVovwel");
+break;
+case 'u':printf("\nVovwel");
+break;
+case 'A':printf("\nVovwel");
+break;
+case 'E':printf("\nVovwel");
+break;
+case 'I':printf("\nVovwel");
+break;
+case 'O':printf("\nVovwel");
+break;
+case 'U':printf("\nVovwel");
+break;
+default:printf("\nIt is not a vovwel");
+}
+getch();
+}
+
