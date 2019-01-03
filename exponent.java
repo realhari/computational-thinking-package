9991b7ce86a1861426a56e56cec04f423582a78a
realhari <43338534+realhari@users.noreply.github.com>
2019-01-03 14:10:59 +0530

    Update exponent.java

--- exponent.java
+++ exponent.java
@@ -1,7 +1,9 @@
 import jva.io.*;
 import java.util.*;
-public class Excersice{
-public static void main(String args[]){
+public class Excersice
+{
+public static void main(String args[])
+{
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter value= ");
 int value=sc.nextInt();
