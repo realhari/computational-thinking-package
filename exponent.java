5ffa3d6af918b54f6058839f439565343ab261ca
realhari <43338534+realhari@users.noreply.github.com>
2019-01-03 14:05:41 +0530

    Create exponent.java

---
+++ exponent.java
@@ -0,0 +1,12 @@
+import jva.io.*;
+import java.util.*;
+public class Excersice{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter value= ");
+int value=sc.nextInt();
+System.out.println("Enter Power value=");
+int power=sc.nextInt();
+System.out.println("OUTPUT="+(value^power));
+}
+}
